b5f0e35e263ed78816516f28822f4235f673f7ec
Mikhail Vetyugov <michaelvetyugov@gmail.com>
2025-08-24 08:04:00 +0300

    add cache to api route

--- app/api/single-issue-stock-data/route.ts
+++ app/api/single-issue-stock-data/route.ts
@@ -1,16 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { LRUCache } from 'lru-cache';
 
 import { fetchSingleIssueData } from '@/services/fetch-single-issue-stock-data';
+
+const responseCache = new LRUCache({
+  max: 50,
+  ttl: 1000 * 60 * 30,
+});
 
 interface IRequestBody {
   ticker: string;
 }
 
 export async function POST(request: NextRequest) {
-  try {
-    const body: IRequestBody = await request.json();
-    const { ticker } = body;
+  const body: IRequestBody = await request.json();
+  const { ticker } = body;
 
+  try {
     if (!ticker) {
       return NextResponse.json(
         { error: 'Ticker is required' },
@@ -20,8 +26,25 @@ export async function POST(request: NextRequest) {
 
     const result = await fetchSingleIssueData(ticker);
 
+    if (!result.price) {
+      throw new Error('No filled price found for the given ticker');
+    }
+
+    responseCache.set(ticker, result);
+
     return NextResponse.json(result);
   } catch (error) {
+    const staleData = responseCache.get(ticker);
+
+    if (staleData) {
+      console.error('Stale data returned due to error:', error);
+
+      return NextResponse.json({
+        ...staleData,
+        stale: true,
+      });
+    }
+
     console.error('Error in API route:', error);
 
     return NextResponse.json(
